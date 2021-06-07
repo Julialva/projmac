4460982185b0e810d45a888affbcb135bb2b81ba
julialva <julio-gris@hotmail.com>
2021-06-06 23:26:47 -0300

    conclude 1.0 BielaManEx

--- Maquinas.py
+++ Maquinas.py
@@ -12,7 +12,6 @@ class Mecanismo:
         x_motor = elos[0]*np.cos(ang)
         y_motor = elos[0]*np.sin(ang)
         x_biela= math.sqrt(elos[1]**2 - (deslocamento - y_motor)**2)
-        print(x_biela)
         fig = plt.figure()
         ax = plt.subplot(111)
         ax.plot([0,x_motor],[0,y_motor])
@@ -21,20 +20,35 @@ class Mecanismo:
         ax.axline((x_motor,y_motor),slope=(y_motor/x_motor),linestyle="--",linewidth=0.5)
         ax.axline(((x_biela+x_motor),deslocamento),slope=((deslocamento-y_motor)/((x_biela+x_motor)-x_motor)),color="purple",linestyle="--",linewidth=0.5)
         if vert:
-            ax.axline(((x_biela+x_motor),deslocamento),slope=90,color="green",linestyle="--",linewidth=0.5)
+            slope=90
+            ax.axline(((x_biela+x_motor),deslocamento),slope=slope,color="green",linestyle="--",linewidth=0.5)
         else:
-            ax.axline(((x_biela+x_motor),deslocamento),slope=0,color="green",linestyle="--",linewidth=0.5)
+            slope=0
+            ax.axline(((x_biela+x_motor),deslocamento),slope=slope,color="green",linestyle="--",linewidth=0.5)
+        b_motor = -x_motor*(y_motor/x_motor) + y_motor
+        #b_biela = -(x_biela+x_motor)*((deslocamento-y_motor)/((x_biela+x_motor)-x_motor)) + deslocamento
+        b_vert_biela = -(x_biela+x_motor)*slope + deslocamento
+        x_intercept= (+b_motor-b_vert_biela)/(slope-(y_motor/x_motor))
+        y_intercept = x_intercept*(y_motor/x_motor) + b_motor
+        ax.plot(x_intercept,y_intercept,"kD")
         plt.show()
-
-#    def Vel_Cir(elos=[],vel_motor=0,vel_in_rpm=True):
-#         if vel_in_rpm:
-#             vel_ang_barra1 = (np.pi()*2*vel_motor)/60 #vel in rad/s
-#         else:
-#             vel_ang_barra1 = vel_motor
-#         vel_ponto = elos[0]*vel_ang_barra1
-#         #cir_ponto =
-#         vel_ang_prox_barra = vel_ponto 
+        motor_cir = math.sqrt((x_motor-x_intercept)**2 + (y_motor-y_intercept)**2)
+        biela_cir = math.sqrt(((x_motor+x_biela)-x_intercept)**2 + (deslocamento-y_intercept)**2)
+        return motor_cir,biela_cir
+    def Vel_Cir(self,motor_cir,elos=[],vel_motor=0,vel_in_rpm=True):
+        if vel_in_rpm:
+            vel_ang_barra1 = (math.pi*2*vel_motor)/60 #vel in rad/s
+        else:
+            vel_ang_barra1 = vel_motor
+        vel_ponto = elos[0]*vel_ang_barra1
+        vel_ang_bc = vel_ponto/motor_cir
+        return vel_ang_bc
+    def v_pistao(self,biela_cir,vel_ang_bc):
+        return vel_ang_bc*(biela_cir/1000)
 mec = Mecanismo()
-print(mec.Calc_Mobilidade(elos=8, num_j_simples=10))
-print(mec.num_Cirs(8,4))
-mec.plot_mec_BielaEx([127,508],225,-127)
+
+motor_cir_module,biela_cir= mec.plot_mec_BielaEx([127,508],225,-127)
+vel_ang_bc = mec.Vel_Cir(motor_cir_module,elos=[127,508],vel_motor=954.96)
+v_pistao = mec.v_pistao(biela_cir,vel_ang_bc)
+print(vel_ang_bc)
+print(v_pistao)
